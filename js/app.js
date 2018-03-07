effbaa6a0fdb337f25e2c7f7db36419d52465c94
Jeffrey Klier <jeffrey.klier@galvanize.com>
2018-03-07 15:43:24 -0700

    solution to base goal

--- js/app.js
+++ js/app.js
@@ -57,4 +57,38 @@
   };
 
   // ADD YOUR CODE HERE
+  let userInputField = document.querySelector('#search')
+  let button = document.querySelector('button[name="action"]')
+
+  button.addEventListener('click', function(e){
+    e.preventDefault()
+    if(userInputField.value === ''){
+      alert('Please enter something into the search box')
+    }
+    else{
+      fetch(`https://omdb-api.now.sh/?s=${userInputField.value}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          while(movies.length > 0){
+            movies.pop()
+          }
+          if(data.Response === "False"){
+            alert('try again lol')
+          }
+          else{
+            for(let i = 0 ; i < data.Search.length ; i++){
+              let newMovie = {
+                id: data.Search[i].imdbID,
+                poster: data.Search[i].Poster,
+                title: data.Search[i].Title,
+                year: data.Search[i].Year
+              }
+              movies.push(newMovie)
+            }
+            renderMovies()
+          }
+        })
+    }
+  })
 })();
